2d2642660189b10575861b004e24a8d12f3f7b3f
bonjourjecode <rayane.waidi45@gmail.com>
2022-05-12 12:43:47 +0200

    login page done

--- app/src/main/java/com/example/mydigitalcampusmobile/LoginPageActivity.java
+++ app/src/main/java/com/example/mydigitalcampusmobile/LoginPageActivity.java
@@ -7,13 +7,9 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.example.mydigitalcampusmobile.databinding.ActivityLoginPageBinding;
 
 public class LoginPageActivity extends AppCompatActivity {
 
@@ -21,6 +17,19 @@ public class LoginPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+
+        TextView username_email = findViewById(R.id.username);
+        TextView username_password = findViewById(R.id.password);
+
+        Button login_btn = findViewById(R.id.login_button);
+        login_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 
 }
