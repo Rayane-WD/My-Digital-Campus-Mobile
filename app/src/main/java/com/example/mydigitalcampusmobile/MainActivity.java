3e6edc49f299e6f1e1546a2bd1b336468b39770b
Mat <mat_thev@outlook.fr>
2022-05-29 16:46:49 +0200

    util

--- app/src/main/java/com/example/mydigitalcampusmobile/MainActivity.java
+++ app/src/main/java/com/example/mydigitalcampusmobile/MainActivity.java
@@ -1,64 +1,14 @@
 package com.example.mydigitalcampusmobile;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.Lifecycle;
-import androidx.viewpager2.adapter.FragmentStateAdapter;
-import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.TableLayout;
-
-import com.google.android.material.tabs.TabLayout;
-import com.google.android.material.tabs.TabLayoutMediator;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-
-    private ArrayList<DayCourses> all_courses = new ArrayList<DayCourses>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-        //On crée des journées de cours
-        DayCourses monday_courses = new DayCourses("Lundi");
-        monday_courses.addCourse(new Course("TD", "Anglais", "MUNN", "B06A"), 1);
-        monday_courses.addCourse(new Course("TP", "Programation weeeeeeeeeeb", "BENABOU", "B14"), 2);
-        monday_courses.addCourse(new Course("TG", "maaaaaaaaaaaaaaaaaaaaaaaaaaaaaaths", "ugkjhdcjjjjjjjfxxxxxxxxxxxxxxx", "B14"), 4);
-        this.all_courses.add(monday_courses);
-
-        DayCourses thuesday_courses = new DayCourses("Mardi");
-        thuesday_courses.addCourse(new Course("TD", "Anglais", "MUNN", "B06A"), 1);
-        thuesday_courses.addCourse(new Course("TP", "Programation web", "BENABOU", "B14"), 4);
-        this.all_courses.add(thuesday_courses);
-
-
-        //On lie notre viewpager2 à son adapter
-        ViewPager2 viewPager2 = findViewById(R.id.schedule_viewpager);
-        viewPager2.setAdapter(
-                new ScheduleAdapter(this, this.all_courses)
-        );
-
-        //Implémentation d'un table layout
-        TabLayout tabLayout = findViewById(R.id.tablayout_id); // On récupère notre table layout depuis la fragment schedule
-        new TabLayoutMediator(
-                tabLayout,
-                viewPager2,
-                new TabLayoutMediator.TabConfigurationStrategy() {
-                    @Override
-                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
-                        tab.setText(all_courses.get(position).getDay());
-                    }
-                }
-        ).attach();
-
     }
 }
---
+++ app/src/main/java/com/example/mydigitalcampusmobile/util/FirebaseUtil.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2021 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.mydigitalcampusmobile.util;
+
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+/**
+ * Utility class for initializing Firebase services and connecting them to the Firebase Emulator
+ * Suite if necessary.
+ */
+public class FirebaseUtil {
+
+    /** Use emulators only in debug builds **/
+
+    private static FirebaseFirestore FIRESTORE;
+
+
+    public static FirebaseFirestore getFirestore() {
+        if (FIRESTORE == null) {
+            FIRESTORE = FirebaseFirestore.getInstance();
+
+            // Connect to the Cloud Firestore emulator when appropriate. The host '10.0.2.2' is a
+            // special IP address to let the Android emulator connect to 'localhost'.
+
+        }
+
+        return FIRESTORE;
+    }
+
+}
